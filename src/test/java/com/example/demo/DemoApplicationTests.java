01597e3884bd0ede97c917e68b511eef8c773054
Darthsmith <piotrkowalik246@gmail.com>
2019-12-06 17:15:03 +0100

    1st commit

---
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
